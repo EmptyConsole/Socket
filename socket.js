d836ab2671e792ee5b033057dc8d9fbccabc389d
toff-tofu <e8623y@gmail.com>
2025-11-17 20:38:57 -0800

    push

--- socket.js
+++ socket.js
@@ -1,40 +1,54 @@
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http, {
-  cors: { origin: "*" }
-});
+const io = require("socket.io")(http, { cors: { origin: "*" } });
 
 const PORT = process.env.PORT || 3000;
 
-// All connected players
-let players = {};
+let players = {};  // <- GLOBAL PLAYER LIST
 
 io.on("connection", (socket) => {
   console.log("Player connected:", socket.id);
 
   // Create new player
-  players[socket.id] = { x: 100, y: 100 };
+  players[socket.id] = { x: 800, y: 800, angle: 0 };
 
-  // Send existing players to the new one
+  // Send full list to newcomer
   socket.emit("initialState", players);
 
-  // Broadcast new player to everyone else
-  socket.broadcast.emit("newPlayer", { id: socket.id, ...players[socket.id] });
+  // Tell everyone else about new player
+  socket.broadcast.emit("newPlayer", {
+    id: socket.id,
+    x: players[socket.id].x,
+    y: players[socket.id].y,
+    angle: 0
+  });
 
-  // Movement updates from client
+  // Receive movement updates
   socket.on("updateData", (data) => {
-    if (!players[socket.id]) return;
-    players[socket.id] = data;
-    io.emit("update", {id: socket.id, data: data});
+    if (players[socket.id]) {
+      players[socket.id].x = data.x;
+      players[socket.id].y = data.y;
+      players[socket.id].angle = data.angle;
+
+      // Send only this player's update to others
+      socket.broadcast.emit("update", {
+        id: socket.id,
+        x: data.x,
+        y: data.y,
+        angle: data.angle
+      });
+    }
   });
 
   // Handle disconnect
   socket.on("disconnect", () => {
     console.log("Player disconnected:", socket.id);
     delete players[socket.id];
-    io.emit("removePlayer", socket.id);
+    socket.broadcast.emit("removePlayer", socket.id);
   });
 });
 
-http.listen(PORT, () => console.log("Server running on", PORT));
+http.listen(PORT, () => {
+  console.log("Server running on port ${PORT}");
+});
