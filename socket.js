bf6f01a658c04fae53f8a112a5f24bc0fbee0d4f
Empty Console <consoleempty@gmail.com>
2025-11-17 21:49:58 -0800

    Update socket.js

--- socket.js
+++ socket.js
@@ -4,8 +4,7 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http, { cors: { origin: "*" } });
 
 const PORT = process.env.PORT || 3000;
-
-let players = {};  // <- GLOBAL PLAYER LIST
+let players = {};
 
 io.on("connection", (socket) => {
   console.log("Player connected:", socket.id);
@@ -16,22 +15,18 @@ io.on("connection", (socket) => {
   // Send full list to newcomer
   socket.emit("initialState", players);
 
-  // Tell everyone else about new player
+  // Notify others
   socket.broadcast.emit("newPlayer", {
     id: socket.id,
-    x: players[socket.id].x,
-    y: players[socket.id].y,
+    x: 800,
+    y: 800,
     angle: 0
   });
 
   // Receive movement updates
   socket.on("updateData", (data) => {
     if (players[socket.id]) {
-      players[socket.id].x = data.x;
-      players[socket.id].y = data.y;
-      players[socket.id].angle = data.angle;
-
-      // Send only this player's update to others
+      players[socket.id] = { x: data.x, y: data.y, angle: data.angle };
       socket.broadcast.emit("update", {
         id: socket.id,
         x: data.x,
@@ -41,14 +36,6 @@ io.on("connection", (socket) => {
     }
   });
 
-  // socket.on("update", (data) => {
-  //   if (players[data.id]) {
-  //     players[data.id].x = data.x;
-  //     players[data.id].y = data.y;
-  //     players[data.id].angle = data.angle;
-  //   }
-  // });
-
   // Handle disconnect
   socket.on("disconnect", () => {
     console.log("Player disconnected:", socket.id);
@@ -57,6 +44,4 @@ io.on("connection", (socket) => {
   });
 });
 
-http.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
